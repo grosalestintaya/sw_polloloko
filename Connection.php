93faf2f703b9df85565ea102f033d13cc493fd44
georgert <grosalestintaya@gmail.com>
2022-09-28 01:16:49 -0500

    eliminado las imagenes inecesarias

--- Connection.php
+++ Connection.php
@@ -1,7 +1,7 @@
 <?php
-	$Host = "localhost";
-	$Username = "root";
-	$Password = "";
+	$Host = "examencloud.cbu56rel5olk.us-east-1.rds.amazonaws.com";
+	$Username = "admin";
+	$Password = "76668813g";
 	$Db = "db_swdp";
 	
 	$Conn = mysqli_connect($Host,$Username,$Password,$Db);
